b18202d34179924ce32c6691f2d4e62f06ae7a64
Dania Meira <meiradania@gmail.com>
2019-06-24 16:23:13 +0200

    api key

--- youtube_videos.py
+++ youtube_videos.py
@@ -2,7 +2,7 @@ from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 
-DEVELOPER_KEY = "AIzaSyA5fovotZJ-50uB0n3hQ9uxeZeHBrfCYoQ"
+DEVELOPER_KEY = ""
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
